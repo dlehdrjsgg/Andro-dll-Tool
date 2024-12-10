cb414291726905bdc5f9bf8c64c9687be6e2c45a
Beef <168471625+dlehdrjsgg@users.noreply.github.com>
2024-12-11 03:28:10 +0900

    Update modify.py

--- modules/analyze/modify.py
+++ modules/analyze/modify.py
@@ -1,17 +1,20 @@
 def modify_binary(file_path, offset, hex_string):
     try:
-        # 바이트로 변환
+        if isinstance(offset, str):
+            offset = int(offset, 16)
         modified = bytes.fromhex(hex_string)
         
         with open(file_path, 'r+b') as binary:
-            # 오프셋 이동
             binary.seek(offset)
-            # 값을 수정 < (그대로)
             binary.write(modified)
-            print(f"Successfully modified offset {hex(offset)} in {file_path}")
+            print(f"Successfully modified offset {hex(offset)} in {file_path}\n")
+            
     except FileNotFoundError:
-        print(f"File not found: {file_path}")
-    except ValueError:
-        print("Invalid hex string. Please provide a valid space-separated hex string.")
+        print(f"File not found: {file_path}\n")
+    except ValueError as e:
+        if "invalid literal" in str(e):
+            print(f"Invalid offset value. Please provide a valid hex or decimal number.\n")
+        else:
+            print("Invalid hex string. Please provide a valid space-separated hex string.\n")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred: {e}\n")
