19f1b22274e27867c8afc2c971258614563e1989
dlehdrjsg <168471625+truebrid@users.noreply.github.com>
2024-12-10 23:59:13 +0900

    DummyDll -> dump.cs

--- modules/dump/Il2CppDumper.py
+++ modules/dump/Il2CppDumper.py
@@ -19,36 +19,43 @@ def run_il2cpp_dumper(lib_path, metadata_path, package_name):
             il2cpp_dumper_dll = os.path.join(il2cpp_dumper_dll, "Il2CppDumper.dll")
             command = ["dotnet", il2cpp_dumper_dll, lib_path, metadata_path]
             process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
-            stdout, stderr = process.communicate(input="\n")  # '\n'을 입력으로 제공하여 'Press any key to exit...' 메시지를 피함
+            stdout, stderr = process.communicate(input="\n")  
             # if stderr:
             #     print(f"Error: {stderr.strip()}")
-            dummy_dll_folder = os.path.join(os.path.dirname(il2cpp_dumper_dll), "DummyDll")
-            destination_folder = os.path.join(output_path, "DummyDll")
-
-            if os.path.exists(dummy_dll_folder):
-                shutil.copytree(dummy_dll_folder, destination_folder)
-                print(f"DummyDll folder copied to: {destination_folder}")
+            #dummy_dll_folder = os.path.join(os.path.dirname(il2cpp_dumper_dll), "DummyDll")
+            #destination_folder = os.path.join(output_path, "DummyDll")
+            dump_cs_file = os.path.join(os.path.dirname(il2cpp_dumper_dll), "dump.cs")
+            destination_file = os.path.join(output_path, "dump.cs")
+            if os.path.exists(dump_cs_file):
+                shutil.copy(dump_cs_file, destination_file)
+                print(f"dump.cs file copied to: {destination_file}\n")
             else:
-                print(f"Error: DummyDll folder not found at {dummy_dll_folder}")
+                print(f"Error: dump.cs file not found at {dump_cs_file}\n")
+
+            # if os.path.exists(dummy_dll_folder):
+            #     shutil.copytree(dummy_dll_folder, destination_folder)
+            #     print(f"DummyDll folder copied to: {destination_folder}\n")
+            # else:
+            #     print(f"Error: DummyDll folder not found at {dummy_dll_folder}\n")
         except Exception as e:
-            print(f"Error running Il2CppDumper: {e}")
+            print(f"Error running Il2CppDumper: {e}\n")
 
     elif system == "Windows":
         try:
             #il2cpp_dumper_dll = os.path.join(il2cpp_dumper_dll, "Il2CppDumper.dll")
             command = ["Il2CppDumper.exe", lib_path, metadata_path]
             process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
-            stdout, stderr = process.communicate(input="\n")  # '\n'을 입력으로 제공하여 'Press any key to exit...' 메시지를 피함
+            stdout, stderr = process.communicate(input="\n")  
             dummy_dll_folder = "path/to/DummyDll"  # 실제 Windows에서 DummyDll 경로로 수정 필요
             destination_folder = os.path.join(output_path, "DummyDll")
 
             if os.path.exists(dummy_dll_folder):
                 shutil.copytree(dummy_dll_folder, destination_folder)
-                print(f"DummyDll folder copied to: {destination_folder}")
+                print(f"DummyDll folder copied to: {destination_folder}\n")
             else:
-                print(f"Error: DummyDll folder not found at {dummy_dll_folder}")
+                print(f"Error: DummyDll folder not found at {dummy_dll_folder}\n")
         except Exception as e:
-            print(f"Error running Il2CppDumper: {e}")
+            print(f"Error running Il2CppDumper: {e}\n")
     
     else:
-        print(f"Unsupported operating system: {system}. Only macOS and Windows are supported.")
+        print(f"[ERROR] Unsupported operating system: {system}. Only macOS and Windows are supported.\n")
